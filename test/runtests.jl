0ce1418e1e3f03ca94c4306210ae0efe96fea087
Jan-Philipp Koester <koester.janphilipp@gmail.com>
2019-10-20 22:34:21 +0200

    Adding test for the functions f1 and d_diff2

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
 using PhomericsTest
 using Test
 
-f_diff2(1,2)
-
 @testset "PhomericsTest.jl" begin
     @test f2(1,2) == 6
     @test f2(10,10) == 40
+    @test f_diff2(1,2) == 2
+    @test f1(1,1) == 2
 end

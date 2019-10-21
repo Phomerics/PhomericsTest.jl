d4055efbfa81c129402972c50fa8f9a3ef325154
Jan-Philipp Koester <koester.janphilipp@gmail.com>
2019-10-21 22:23:12 +0200

    Added some new functions and a corresponding test

---
+++ src/CalcD.jl
@@ -0,0 +1,73 @@
+function DX(delta, Nx, Ny)
+    # This function creates a finite difference operator which represents a
+    # first order x-derivitive.
+    # INPUT
+    # delta: resolution of the operator - scalar / a.u.
+    # Nx: Number of elements in x-direction - integer / #
+    # Ny: Number of elements in y-direction - integer / #
+    # OUTPUT
+    # operator:  (Nx*Ny X Nx*Ny)-Dim. FD-Dx operator - array / a.u.
+    N = Nx*Ny
+    next_vec = fill(1.0/(2.0*delta), N-1)
+    if Ny > 1
+        for ii in Nx:Nx:N-1 next_vec[ii] = 0.0 end
+    end
+    next_sparse_mat = sparse(2:N, 1:N-1, next_vec, N, N)
+    operator = next_sparse_mat' - next_sparse_mat
+    return operator
+end
+
+function DXX(delta, Nx, Ny)
+    # This function creates a finite difference operator which represents a
+    # second order x-derivitive.
+    # INPUT
+    # delta: resolution of the operator - scalar / a.u.
+    # Nx: Number of elements in x-direction - integer / #
+    # Ny: Number of elements in y-direction - integer / #
+    # OUTPUT
+    # operator:  (Nx*Ny X Nx*Ny)-Dim. FD-Dxx operator - array / a.u.
+    N = Nx*Ny
+    main_vec = fill(-2.0 /delta^2, N)
+    next_vec = fill(1.0/delta^2, N-1)
+    if Ny > 1
+        for ii in Nx:Nx:(N-1) next_vec[ii] = 0.0 end
+    end
+    main_sparse_mat = sparse(1:N, 1:N, main_vec, N, N)
+    next_sparse_mat = sparse(2:N, 1:(N-1), next_vec, N, N)
+    operator = next_sparse_mat' + next_sparse_mat + main_sparse_mat
+    return operator
+end
+
+function DY(delta, Nx, Ny)
+    # This function creates a finite difference operator which represents a
+    # first order y-derivitive.
+    # INPUT
+    # delta: resolution of the operator - scalar / a.u.
+    # Nx: Number of elements in x-direction - integer / #
+    # Ny: Number of elements in y-direction - integer / #
+    # OUTPUT
+    # operator:  (Nx*Ny X Nx*Ny)-Dim. FD-Dy operator - array / a.u.
+    N = Nx*Ny
+    next_vec = fill(1.0/delta^2, (N-Ny))
+    next_sparse_mat = sparse((1+Ny):N, 1:(N-Ny), next_vec, N, N)
+    operator = next_sparse_mat' - next_sparse_mat
+    return operator
+end
+
+function DYY(delta, Nx, Ny)
+    # This function creates a finite difference operator which represents a
+    # second order y-derivitive.
+    # INPUT
+    # delta: resolution of the operator - scalar / a.u.
+    # Nx: Number of elements in x-direction - integer / #
+    # Ny: Number of elements in y-direction - integer / #
+    # OUTPUT
+    # operator:  (Nx*Ny X Nx*Ny)-Dim. FD-Dyy operator - array / a.u.
+    N = Nx*Ny
+    main_vec = fill(-2.0/delta^2, N)
+    next_vec = fill(1.0/delta^2, (N-Ny))
+    main_sparse_mat = sparse(1:N, 1:N, main_vec, N, N)
+    next_sparse_mat = sparse((1+Ny):N, 1:(N-Ny), next_vec, N, N)
+    operator = next_sparse_mat + next_sparse_mat' + main_sparse_mat
+    return operator
+end
--- src/PhomericsTest.jl
+++ src/PhomericsTest.jl
@@ -1,11 +1,14 @@
 module PhomericsTest
 
-using ForwardDiff
+using ForwardDiff    # used in my_test_functions.jl
+using SparseArrays   # used in CalcD.jl
+
 
 include("my_test_functions.jl")
-
 export f1
 export f2
 export f_diff2
+
+include("CalcD.jl")
 
 end # module

82930bdfd9ad1b41154aaaf322eca1db777e6094
Jan-Philipp Koester <koester.janphilipp@gmail.com>
2019-10-21 21:41:18 +0200

    Adding SafeTestsets.jl

--- src/my_test_functions.jl
+++ src/my_test_functions.jl
@@ -1,4 +1,3 @@
 f1(x,y) = x + y
 f2(x,y) = 2x + 2y
-
-f_diff2(x,y) = ForwardDiff.derivative(x->f2(x,2),x)
+f_diff2(x,y) = ForwardDiff.derivative(x->f2(x,y),x)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,2 @@
-using PhomericsTest
-using Test
-
-@testset "PhomericsTest.jl" begin
-    @test f2(1,2) == 6
-    @test f2(10,10) == 40
-    @test f_diff2(1,2) == 2
-    @test f1(1,1) == 2
-end
+using SafeTestsets
+@safetestset "my f tests" begin include("f_tests.jl") end

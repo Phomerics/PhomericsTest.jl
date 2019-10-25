6b10b71393188d857ba638fc0a9e308872738224
Jan-Philipp Koester <56580022+Phomerics@users.noreply.github.com>
2019-10-25 09:13:36 +0200

    Added the test set Tools_test.jl to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
 using SafeTestsets
 @safetestset "my f tests" begin include("f_tests.jl") end
 @safetestset "my Operators tests" begin include("Operators_tests.jl") end
+@safetestset "my Operators tests" begin include("Tools_tests.jl") end
